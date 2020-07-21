a6369db8cc91380b7cef1350808d32c627569845
Scott Nichols <snichols@vmware.com>
2020-07-21 10:25:55 -0700

    Pure bootstrap+rename from sample-controller

---
+++ pkg/reconciler/ducktype/controller.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ducktype
+
+import (
+	"context"
+
+	"knative.dev/pkg/tracker"
+
+	corev1 "k8s.io/api/core/v1"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/logging"
+
+	svcinformer "knative.dev/pkg/client/injection/kube/informers/core/v1/service"
+	ducktypeinformer "knative.dev/discovery/pkg/client/injection/informers/discovery/v1alpha1/ducktype"
+	ducktypereconciler "knative.dev/discovery/pkg/client/injection/reconciler/discovery/v1alpha1/ducktype"
+)
+
+// NewController creates a Reconciler and returns the result of NewImpl.
+func NewController(
+	ctx context.Context,
+	cmw configmap.Watcher,
+) *controller.Impl {
+	logger := logging.FromContext(ctx)
+
+	ducktypeInformer := ducktypeinformer.Get(ctx)
+	svcInformer := svcinformer.Get(ctx)
+
+	r := &Reconciler{
+		ServiceLister: svcInformer.Lister(),
+	}
+	impl := ducktypereconciler.NewImpl(ctx, r)
+	r.Tracker = tracker.New(impl.EnqueueKey, controller.GetTrackerLease(ctx))
+
+	logger.Info("Setting up event handlers.")
+
+	ducktypeInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
+
+	svcInformer.Informer().AddEventHandler(controller.HandleAll(
+		// Call the tracker's OnChanged method, but we've seen the objects
+		// coming through this path missing TypeMeta, so ensure it is properly
+		// populated.
+		controller.EnsureTypeMeta(
+			r.Tracker.OnChanged,
+			corev1.SchemeGroupVersion.WithKind("Service"),
+		),
+	))
+
+	return impl
+}
