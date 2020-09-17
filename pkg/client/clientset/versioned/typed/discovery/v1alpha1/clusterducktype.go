3bee81642e9ea1e86b1df4f3e575cb56675f5cee
Scott Nichols <snichols@vmware.com>
2020-09-17 13:40:45 -0700

    target k8s 1.18 (#61)

--- pkg/client/clientset/versioned/typed/discovery/v1alpha1/clusterducktype.go
+++ pkg/client/clientset/versioned/typed/discovery/v1alpha1/clusterducktype.go
@@ -19,6 +19,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"time"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -37,15 +38,15 @@ type ClusterDuckTypesGetter interface {
 
 // ClusterDuckTypeInterface has methods to work with ClusterDuckType resources.
 type ClusterDuckTypeInterface interface {
-	Create(*v1alpha1.ClusterDuckType) (*v1alpha1.ClusterDuckType, error)
-	Update(*v1alpha1.ClusterDuckType) (*v1alpha1.ClusterDuckType, error)
-	UpdateStatus(*v1alpha1.ClusterDuckType) (*v1alpha1.ClusterDuckType, error)
-	Delete(name string, options *v1.DeleteOptions) error
-	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
-	Get(name string, options v1.GetOptions) (*v1alpha1.ClusterDuckType, error)
-	List(opts v1.ListOptions) (*v1alpha1.ClusterDuckTypeList, error)
-	Watch(opts v1.ListOptions) (watch.Interface, error)
-	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ClusterDuckType, err error)
+	Create(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.CreateOptions) (*v1alpha1.ClusterDuckType, error)
+	Update(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.UpdateOptions) (*v1alpha1.ClusterDuckType, error)
+	UpdateStatus(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.UpdateOptions) (*v1alpha1.ClusterDuckType, error)
+	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
+	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha1.ClusterDuckType, error)
+	List(ctx context.Context, opts v1.ListOptions) (*v1alpha1.ClusterDuckTypeList, error)
+	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ClusterDuckType, err error)
 	ClusterDuckTypeExpansion
 }
 
@@ -62,19 +63,19 @@ func newClusterDuckTypes(c *DiscoveryV1alpha1Client) *clusterDuckTypes {
 }
 
 // Get takes name of the clusterDuckType, and returns the corresponding clusterDuckType object, and an error if there is any.
-func (c *clusterDuckTypes) Get(name string, options v1.GetOptions) (result *v1alpha1.ClusterDuckType, err error) {
+func (c *clusterDuckTypes) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ClusterDuckType, err error) {
 	result = &v1alpha1.ClusterDuckType{}
 	err = c.client.Get().
 		Resource("clusterducktypes").
 		Name(name).
 		VersionedParams(&options, scheme.ParameterCodec).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of ClusterDuckTypes that match those selectors.
-func (c *clusterDuckTypes) List(opts v1.ListOptions) (result *v1alpha1.ClusterDuckTypeList, err error) {
+func (c *clusterDuckTypes) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.ClusterDuckTypeList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -84,13 +85,13 @@ func (c *clusterDuckTypes) List(opts v1.ListOptions) (result *v1alpha1.ClusterDu
 		Resource("clusterducktypes").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Watch returns a watch.Interface that watches the requested clusterDuckTypes.
-func (c *clusterDuckTypes) Watch(opts v1.ListOptions) (watch.Interface, error) {
+func (c *clusterDuckTypes) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -100,81 +101,84 @@ func (c *clusterDuckTypes) Watch(opts v1.ListOptions) (watch.Interface, error) {
 		Resource("clusterducktypes").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Watch()
+		Watch(ctx)
 }
 
 // Create takes the representation of a clusterDuckType and creates it.  Returns the server's representation of the clusterDuckType, and an error, if there is any.
-func (c *clusterDuckTypes) Create(clusterDuckType *v1alpha1.ClusterDuckType) (result *v1alpha1.ClusterDuckType, err error) {
+func (c *clusterDuckTypes) Create(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.CreateOptions) (result *v1alpha1.ClusterDuckType, err error) {
 	result = &v1alpha1.ClusterDuckType{}
 	err = c.client.Post().
 		Resource("clusterducktypes").
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(clusterDuckType).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Update takes the representation of a clusterDuckType and updates it. Returns the server's representation of the clusterDuckType, and an error, if there is any.
-func (c *clusterDuckTypes) Update(clusterDuckType *v1alpha1.ClusterDuckType) (result *v1alpha1.ClusterDuckType, err error) {
+func (c *clusterDuckTypes) Update(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.UpdateOptions) (result *v1alpha1.ClusterDuckType, err error) {
 	result = &v1alpha1.ClusterDuckType{}
 	err = c.client.Put().
 		Resource("clusterducktypes").
 		Name(clusterDuckType.Name).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(clusterDuckType).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // UpdateStatus was generated because the type contains a Status member.
 // Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-
-func (c *clusterDuckTypes) UpdateStatus(clusterDuckType *v1alpha1.ClusterDuckType) (result *v1alpha1.ClusterDuckType, err error) {
+func (c *clusterDuckTypes) UpdateStatus(ctx context.Context, clusterDuckType *v1alpha1.ClusterDuckType, opts v1.UpdateOptions) (result *v1alpha1.ClusterDuckType, err error) {
 	result = &v1alpha1.ClusterDuckType{}
 	err = c.client.Put().
 		Resource("clusterducktypes").
 		Name(clusterDuckType.Name).
 		SubResource("status").
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(clusterDuckType).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Delete takes name of the clusterDuckType and deletes it. Returns an error if one occurs.
-func (c *clusterDuckTypes) Delete(name string, options *v1.DeleteOptions) error {
+func (c *clusterDuckTypes) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	return c.client.Delete().
 		Resource("clusterducktypes").
 		Name(name).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // DeleteCollection deletes a collection of objects.
-func (c *clusterDuckTypes) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+func (c *clusterDuckTypes) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
 	var timeout time.Duration
-	if listOptions.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOptions.TimeoutSeconds) * time.Second
+	if listOpts.TimeoutSeconds != nil {
+		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
 	}
 	return c.client.Delete().
 		Resource("clusterducktypes").
-		VersionedParams(&listOptions, scheme.ParameterCodec).
+		VersionedParams(&listOpts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // Patch applies the patch and returns the patched clusterDuckType.
-func (c *clusterDuckTypes) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ClusterDuckType, err error) {
+func (c *clusterDuckTypes) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ClusterDuckType, err error) {
 	result = &v1alpha1.ClusterDuckType{}
 	err = c.client.Patch(pt).
 		Resource("clusterducktypes").
-		SubResource(subresources...).
 		Name(name).
+		SubResource(subresources...).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(data).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
