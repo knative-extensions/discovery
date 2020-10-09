e8af21e473a2effe1f8f08813d452c59a27ca897
Scott Nichols <snichols@vmware.com>
2020-10-09 14:33:05 -0700

    leverage test-infra go_update_deps

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -14,45 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly ROOT_DIR=$(dirname $0)/..
-source ${ROOT_DIR}/vendor/knative.dev/test-infra/scripts/library.sh
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-cd ${ROOT_DIR}
+source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
 
-# Parse flags to determine any we should pass to dep.
-# We need these flags for things to work properly.
-export GO111MODULE=on
-export GOFLAGS=-mod=vendor
-
-UPGRADE=0
-VERSION="v0.19"
-while [[ $# -ne 0 ]]; do
-  parameter=$1
-  case ${parameter} in
-    --upgrade) UPGRADE=1 ;;
-    --release) shift; VERSION="$1" ;;
-    *) abort "unknown option ${parameter}" ;;
-  esac
-  shift
-done
-readonly UPGRADE
-readonly VERSION
-
-if (( UPGRADE )); then
-  FLOATING_DEPS=( $(run_go_tool tableflip.dev/buoy buoy float ${ROOT_DIR}/go.mod --release ${VERSION} --domain knative.dev) )
-  echo "Upgrading deps to ${FLOATING_DEPS[@]}"
-  go get -d ${FLOATING_DEPS[@]}
-fi
-
-# Prune modules.
-go mod tidy
-go mod vendor
-
-rm -rf $(find vendor/ -name 'OWNERS')
-rm -rf $(find vendor/ -name '*_test.go')
-
-update_licenses third_party/VENDOR-LICENSE "./..."
+go_update_deps "$@"
