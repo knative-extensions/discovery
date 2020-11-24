40e88d87d2a9e29198581a1c246e12292287ba8a
Knative Automation <automation@knative.team>
2020-11-24 13:42:05 +0000

    upgrade to latest dependencies
    Signed-off-by: Knative Automation <automation@knative.team>

--- go.mod
+++ go.mod
@@ -3,22 +3,26 @@ module knative.dev/discovery
 go 1.14
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cucumber/godog v0.10.0
 	github.com/cucumber/messages-go/v10 v10.0.3
 	github.com/google/go-cmp v0.5.2
 	github.com/google/licenseclassifier v0.0.0-20200708223521-3d09a0ea2f39
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/prometheus/procfs v0.0.11 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
 	gopkg.in/yaml.v2 v2.3.0
-	k8s.io/api v0.18.8
-	k8s.io/apiextensions-apiserver v0.18.8
-	k8s.io/apimachinery v0.18.8
+	k8s.io/api v0.18.12
+	k8s.io/apiextensions-apiserver v0.18.12
+	k8s.io/apimachinery v0.18.12
 	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
-	k8s.io/code-generator v0.18.8
+	k8s.io/code-generator v0.18.12
 	k8s.io/kube-openapi v0.0.0-20200410145947-bcb3869e6f29
-	knative.dev/hack v0.0.0-20201112185459-01a34c573bd8
-	knative.dev/pkg v0.0.0-20201113182900-615f17a0eb05
-	knative.dev/reconciler-test v0.0.0-20201113174300-2b45d4c2e686
+	knative.dev/hack v0.0.0-20201120192952-353db687ec5b
+	knative.dev/pkg v0.0.0-20201124023135-49e55c9eb816
+	knative.dev/reconciler-test v0.0.0-20201123224957-57547e427baf
 )
 
 replace (
--- vendor/knative.dev/reconciler-test/pkg/k8s/wait.go
+++ vendor/knative.dev/reconciler-test/pkg/k8s/wait.go
@@ -19,9 +19,14 @@ package k8s
 import (
 	"context"
 	"fmt"
+	"testing"
+
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"knative.dev/pkg/apis"
 	"knative.dev/pkg/injection/clients/dynamicclient"
+	pkgtest "knative.dev/pkg/test"
+
 	"log"
 	"time"
 
@@ -33,6 +38,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	kubeclient "knative.dev/pkg/client/injection/kube/client"
+
+	"knative.dev/reconciler-test/pkg/environment"
 )
 
 func WaitForReadyOrDone(ctx context.Context, ref corev1.ObjectReference, interval, timeout time.Duration) error {
@@ -64,7 +71,7 @@ func WaitForResourceReady(ctx context.Context, namespace, name string, gvr schem
 	return wait.PollImmediate(interval, timeout, func() (bool, error) {
 		client := dynamicclient.Get(ctx)
 
-		us, err := client.Resource(gvr).Namespace(namespace).Get(context.Background(), name, metav1.GetOptions{})
+		us, err := client.Resource(gvr).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
 		if err != nil {
 			if apierrors.IsNotFound(err) {
 				log.Println(namespace, name, "not found", err)
@@ -80,15 +87,55 @@ func WaitForResourceReady(ctx context.Context, namespace, name string, gvr schem
 		obj.ResourceVersion = gvr.Version
 		obj.APIVersion = gvr.GroupVersion().String()
 
+		// Ready type.
 		ready := obj.Status.GetCondition(apis.ConditionReady)
-		if ready != nil && !ready.IsTrue() {
-			msg := fmt.Sprintf("%s is not ready, %s: %s", name, ready.Reason, ready.Message)
-			if msg != lastMsg {
-				log.Println(msg)
-				lastMsg = msg
+		if ready != nil {
+			// Succeeded type.
+			ready = obj.Status.GetCondition(apis.ConditionSucceeded)
+		}
+		// Test Ready or Succeeded.
+		if ready != nil {
+			if !ready.IsTrue() {
+				msg := fmt.Sprintf("%s is not %s, %s: %s", name, ready.Type, ready.Reason, ready.Message)
+				if msg != lastMsg {
+					log.Println(msg)
+					lastMsg = msg
+				}
 			}
+
+			log.Printf("%s is %s, %s: %s\n", name, ready.Type, ready.Reason, ready.Message)
+			return ready.IsTrue(), nil
 		}
 
-		return ready.IsTrue(), nil
+		// Last resort, look at all conditions.
+		// As a side-effect of this test,
+		//   if a resource has no conditions, then it is ready.
+		allReady := true
+		for _, c := range obj.Status.Conditions {
+			if !c.IsTrue() {
+				msg := fmt.Sprintf("%s is not %s, %s: %s", name, c.Type, c.Reason, c.Message)
+				if msg != lastMsg {
+					log.Println(msg)
+					lastMsg = msg
+				}
+				allReady = false
+				break
+			}
+		}
+		return allReady, nil
 	})
+}
+
+// WaitForServiceEndpointsOrFail wraps the utility from pkg and uses the context to extract kubeclient and namespace
+func WaitForServiceEndpointsOrFail(ctx context.Context, tb testing.TB, svcName string, numberOfExpectedEndpoints int) {
+	if err := pkgtest.WaitForServiceEndpoints(ctx, kubeclient.Get(ctx), svcName, environment.FromContext(ctx).Namespace(), numberOfExpectedEndpoints); err != nil {
+		tb.Fatalf("Failed while waiting for %d endpoints in service %s: %+v", numberOfExpectedEndpoints, svcName, errors.WithStack(err))
+	}
+}
+
+// WaitForPodRunningOrFail wraps the utility from pkg and uses the context to extract kubeclient and namespace
+func WaitForPodRunningOrFail(ctx context.Context, tb testing.TB, podName string) {
+	if err := pkgtest.WaitForPodRunning(ctx, kubeclient.Get(ctx), podName, environment.FromContext(ctx).Namespace()); err != nil {
+		tb.Fatalf("Failed while waiting for pod %s running: %+v", podName, errors.WithStack(err))
+	}
 }
